6f79dfd02db0eb468c5fb5509dd1cd5e21b4185b
Fred Heiberger <Dev3@Freds-MacBook-Pro-2.local>
2015-05-01 14:03:19 -0700

    put inside funciton

--- playnote.js
+++ playnote.js
@@ -1,19 +1,21 @@
-var pianoSound = SoundCache.getSound("https://s3.amazonaws.com/hifi-public/sounds/doorbell.wav");
+(function() {
+  var pianoSound = SoundCache.getSound("https://s3.amazonaws.com/hifi-public/sounds/doorbell.wav");
 
-this.clickDownOnEntity = function(entityID, mouseEvent) {
-  this.entityId = entityId;
-  if (mouseEvent.isLeftButton) {
-    this.ding()
+  this.clickDownOnEntity = function(entityID, mouseEvent) {
+    this.entityId = entityId;
+    if (mouseEvent.isLeftButton) {
+      this.ding();
+    }
   }
-}
 
-this.ding = function(){
-  var position = Entities.getEntityProperties(this.entityId).position
-  if (pianoSound && pianoSound.downloaded) {
-    Audio.playSound(pianoSound, {
-      position: position
-    });
-  } else {
-    print("COULD NOT PLAY SOUND!");
+  this.ding = function(){
+    var position = Entities.getEntityProperties(this.entityId).position
+    if (pianoSound && pianoSound.downloaded) {
+      Audio.playSound(pianoSound, {
+        position: position
+      });
+    } else {
+      print("COULD NOT PLAY SOUND!");
+    }
   }
-}
+})
